b67aaf0a2f0c3a95901ddca26a04fb5b9162de03
Tai <thaivinhtai@gmail.com>
2019-03-01 16:25:18 +0700

    update

--- rsync.py
+++ rsync.py
@@ -1,9 +1,79 @@
 #!/usr/bin/env python3
 import argparse
 import difflib
-from os import (path, open, read, write, sendfile, lseek, mkdir, stat, O_RDONLY,
-                symlink, link, readlink, scandir, unlink, utime, chmod, fdopen)
-from sys import argv
+from os import (path, open, read, write, sendfile, lseek,
+                mkdir, stat, O_RDONLY, symlink, link, readlink,
+                scandir, unlink, utime, chmod, fdopen)
+
+
+class Get_args():
+    """
+    This class uses argparse for get args and do some stuffs with them to get
+    data for progressing.
+    """
+
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('source_files', nargs='*')
+    PARSER.add_argument('destination_file', nargs='?')
+    PARSER.add_argument('-h', '--help',
+                        action='store_true')
+    PARSER.add_argument('-f', '--update',
+                        action='store_true', default=False)
+    PARSER.add_argument('-c', '--checksum',
+                        action='store_true', default=False)
+    PARSER.add_argument('-H', '--hard_links',
+                        action='store_true', default=True)
+    PARSER.add_argument('-l', '--links',
+                        action='store_true', default=True)
+    ARGS = PARSER.parse_args()
+
+    def __init__(self):
+        self.source_files = ARGS.source_files
+        self.invalid_files = self.invalid_source_files(self.source_files)
+        self.dest_file = ARGS.destination_file
+        self.h_option = ARGS.help
+        self.u_option = ARGS.update
+        self.c_option = ARGS.checksum
+        self.H_option = ARGS.hard_links
+        self.l_option = ARGS.links
+        self.hardlink_files = dict()
+
+    def check_path_file_type(self, path_file):
+        """
+        Check if the path is file, directory, symlink
+        """
+        full_path = str(path_file)
+        if full_path[0] == '~':
+            full_path = path.expanduser(full_path)
+        else:
+            full_path = path.realpath(full_path)
+        if path.isfile(full_path):
+            return "file"
+        if path.isdir(full_path):
+            return "directory"
+        if path.islink(full_path):
+            return "symlink"
+        return 0
+
+    def invalid_source_files(self, list_files):
+        """
+        Check if the source files are not exist and group them into a list
+        """
+        invalid_files = []
+        if len(list_files) > 0:
+            # for file in self.source_files:
+            #     if self.check_path_file_type(file) == 0:
+            #         invalid_files.append(file)
+            invalid_files = [file for file in list_files
+                             if self.check_path_file_type(file) == 0]
+        return invalid_files
+
+    def which_files_hardlink(self, list_files):
+        """
+        Group files have same INODE together in a dictionary,
+        which INODE is key and value is list of files.
+        """
+        groups_of_hardlinks = {}
 
 
 def introduction():
@@ -16,47 +86,16 @@ def introduction():
     return introduction_file.read()
 
 
-def check_path_file_type(path_file):
-    """
-    Check if the path is file, directory, symlink
-    """
-    full_path = str(path_file)
-    if full_path[0] == '~':
-        full_path = path.expanduser(full_path)
-    else:
-        full_path = path.realpath(full_path)
-    if path.isfile(full_path):
-        return "file"
-    if path.isdir(full_path):
-        return "directory"
-    if path.islink(full_path):
-        return "symlink"
-
-
-def which_files_hardlink(*args):
-    """
-
-    """
-
-
 def main():
     """
     Main funtion
     """
-    if (len(argv) == 1) or ('-h' in argv) or ('--help' in argv):
+    argv = Get_args()
+    if (len(argv.source_files) == 0) or (argv.help):
         print(introduction())
         return 0
-    print("hello " + argv[1])
+    print("Fail")
 
 
 if __name__ == "__main__":
     main()
-# import argparse
-#
-# parser = argparse.ArgumentParser(description="Example for store true & false")
-# parser.add_argument("--set-false", "--s", "--sf", help="Set var_name to false", dest="var_name", action="store_false")
-# parser.set_defaults(var_name=True)
-#
-# args = parser.parse_args()
-#
-# print ("var_name is %s" % args.var_name)
--- test.py
+++ test.py
@@ -3,17 +3,20 @@
 
 import argparse
 # Initialize parser with a short description
-parser = argparse.ArgumentParser(description='Compress a file using various algorithms')
+parser = argparse.ArgumentParser()
 
 # Add positional and optional arguments
-parser.add_argument('input_file', nargs='+', help='file to be compressed')
-parser.add_argument('output_file', help='compressed output file')
-parser.add_argument('--alg', help='algorithm will be used')
+parser.add_argument('input_file', nargs='*', help='file to be compressed')
+parser.add_argument('output_file', nargs='?', help='compressed output file')
+parser.add_argument('--alg', help='algorithm will be used', default="Empty")
+parser.add_argument('-H', '--hard_links', action='store_true', default=True)
 # Parse argument
 args = parser.parse_args()
+
 # if not vars(args):
 #     parser.print_help()
 #     parser.exit(1)
 print('Input file: {}'.format(args.input_file))
 print('Output file: {}'.format(args.output_file))
 print('Algorithm: {}'.format(args.alg))
+print('Algorithm: {}'.format(args.hard_links))
